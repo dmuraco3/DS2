8ab7867fe2f76e6bea54e704010a276490ade631
Dylan Muraco <dylanjmuraco@gmail.com>
2025-01-21 14:46:59 -0500

    refactoring dylan.java

--- src/dylan.java
+++ src/dylan.java
@@ -1,6 +1,7 @@
 class dylan {
     public static void main (String args[]) {
         String awesomeName = "Dylan Muraco";
-        System.out.println(awesomeName);
+        String postfix = " is awesome";
+        System.out.println(awesomeName + postfix);
     }
 }
