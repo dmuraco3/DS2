5abddc15759c79ec729526714efea308c72dd99c
Dylan Muraco <dylanjmuraco@gmail.com>
2025-01-28 13:50:23 -0500

    removing

--- src/dylan.java
+++
@@ -1,7 +0,0 @@
-class dylan {
-    public static void main (String args[]) {
-        String awesomeName = "Dylan Muraco";
-        String postfix = " is cool";
-        System.out.println(awesomeName + postfix);
-    }
-}
